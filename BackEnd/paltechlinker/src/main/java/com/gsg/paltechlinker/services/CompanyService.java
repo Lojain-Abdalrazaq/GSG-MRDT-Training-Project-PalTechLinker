1195b0f6ba4ebfc91e414a1540e9518dc53d064b
Mays-Alreem <hroubmays@gmail.com>
2024-09-26 07:43:56 +0300

    feat: add service interface and implementation for company

---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/CompanyService.java
@@ -0,0 +1,7 @@
+package com.gsg.paltechlinker.services;
+
+public interface CompanyService {
+    
+    
+
+}
---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/impl/CompanyServiceImpl.java
@@ -0,0 +1,7 @@
+package com.gsg.paltechlinker.services.impl;
+
+import com.gsg.paltechlinker.services.CompanyService;
+
+public class CompanyServiceImpl implements CompanyService {
+    
+}
