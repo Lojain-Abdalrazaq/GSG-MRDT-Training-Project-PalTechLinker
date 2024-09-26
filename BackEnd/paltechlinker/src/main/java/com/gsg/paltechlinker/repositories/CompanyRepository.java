e505f2e77e0b686e8a64a2fca33c50d9b29f7c7e
Mays-Alreem <hroubmays@gmail.com>
2024-09-26 15:24:24 +0300

    feat: add pagination feature to CompanyRepository

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/repositories/CompanyRepository.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/repositories/CompanyRepository.java
@@ -1,10 +1,12 @@
 package com.gsg.paltechlinker.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import com.gsg.paltechlinker.domain.entities.CompanyEntity;
 
 @Repository
-public interface CompanyRepository extends CrudRepository<CompanyEntity, Long> {
+public interface CompanyRepository extends CrudRepository<CompanyEntity, Long>,
+                                            PagingAndSortingRepository<CompanyEntity, Long> {    
 
 }
