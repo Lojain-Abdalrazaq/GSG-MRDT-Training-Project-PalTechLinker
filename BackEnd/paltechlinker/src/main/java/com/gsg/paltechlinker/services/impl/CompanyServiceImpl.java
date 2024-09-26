7b3ad161e3979454f35d81b87fe0e6a909627d19
Mays-Alreem <hroubmays@gmail.com>
2024-09-26 07:45:44 +0300

    feat: inject company-repository in company-service

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/impl/CompanyServiceImpl.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/impl/CompanyServiceImpl.java
@@ -1,7 +1,16 @@
 package com.gsg.paltechlinker.services.impl;
 
+import org.springframework.stereotype.Service;
+import com.gsg.paltechlinker.repositories.CompanyRepository;
 import com.gsg.paltechlinker.services.CompanyService;
 
+@Service
 public class CompanyServiceImpl implements CompanyService {
+
+    private CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
     
 }
