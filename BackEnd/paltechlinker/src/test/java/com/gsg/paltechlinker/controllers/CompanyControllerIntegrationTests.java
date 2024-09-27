7e898c9d41d640ee0d8a21df6c02e44d2b899639
Mays-Alreem <hroubmays@gmail.com>
2024-09-27 21:53:16 +0300

    test: verify that create-company returns HTTP status 201

--- BackEnd/paltechlinker/src/test/java/com/gsg/paltechlinker/controllers/CompanyControllerIntegrationTests.java
+++ BackEnd/paltechlinker/src/test/java/com/gsg/paltechlinker/controllers/CompanyControllerIntegrationTests.java
@@ -1,11 +1,18 @@
 package com.gsg.paltechlinker.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gsg.paltechlinker.TestDataUtil;
+import com.gsg.paltechlinker.domain.entities.CompanyEntity;
 import com.gsg.paltechlinker.services.CompanyService;
 
 
@@ -23,6 +30,20 @@ public class CompanyControllerIntegrationTests {
         this.mockMvc = mockMvc;
         this.companyService = companyService;
         this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateCompanyReturnsHttpStatus201Created() throws Exception {
+        CompanyEntity companyEntity = TestDataUtil.createTestCompanyEntityA();
+        companyEntity.setId(null);
+        String companyJson = objectMapper.writeValueAsString(companyEntity);
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/companies/create")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(companyJson)
+        ).andExpect(
+            MockMvcResultMatchers.status().isCreated()
+        );
     }
 
 }
