4622b9f152ecd5703ae7eaf6df7403784760008d
Mays-Alreem <hroubmays@gmail.com>
2024-09-27 10:33:37 +0300

    feat: add DELETE endpoint to delete company with given id

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/CompanyController.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/CompanyController.java
@@ -5,23 +5,18 @@ import com.gsg.paltechlinker.domain.dto.CompanyDto;
 import com.gsg.paltechlinker.domain.entities.CompanyEntity;
 import com.gsg.paltechlinker.mappers.Mapper;
 import com.gsg.paltechlinker.services.CompanyService;
-
 import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
-
 
 
 @RestController
@@ -81,6 +76,12 @@ public class CompanyController {
         CompanyEntity companyEntityWithNewData = mapper.mapFrom(companyDtoWithNewData);
         CompanyEntity  updatedCompanyEntity = companyService.partialUpdate(id, companyEntityWithNewData);
         return new ResponseEntity<>(mapper.mapTo(updatedCompanyEntity), HttpStatus.OK);
+    }
+
+    @DeleteMapping(COMPANIES_ID)
+    public ResponseEntity deleteCompany(@PathVariable Long id) {
+        companyService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
