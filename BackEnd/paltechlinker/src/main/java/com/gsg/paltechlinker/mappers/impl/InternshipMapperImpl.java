701fe90bb184f96e8027bff413afc8a3cd1fd4e1
Mays-Alreem <hroubmays@gmail.com>
2024-09-28 20:48:44 +0300

    feat: implement an entity-dto-mapper for internship

---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/mappers/impl/InternshipMapperImpl.java
@@ -0,0 +1,29 @@
+package com.gsg.paltechlinker.mappers.impl;
+
+import org.modelmapper.ModelMapper;
+
+import com.gsg.paltechlinker.domain.dto.InternshipDto;
+import com.gsg.paltechlinker.domain.entities.InternshipEntity;
+import com.gsg.paltechlinker.mappers.Mapper;
+
+public class InternshipMapperImpl implements Mapper<InternshipEntity, InternshipDto> {
+
+    private ModelMapper modelMapper;
+
+    public InternshipMapperImpl(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public InternshipDto mapTo(InternshipEntity internshipEntity) {
+        return modelMapper.map(internshipEntity, InternshipDto.class);
+    }
+
+    @Override
+    public InternshipEntity mapFrom(InternshipDto internshipDto) {
+        return modelMapper.map(internshipDto, InternshipEntity.class);
+    }
+
+    
+}
