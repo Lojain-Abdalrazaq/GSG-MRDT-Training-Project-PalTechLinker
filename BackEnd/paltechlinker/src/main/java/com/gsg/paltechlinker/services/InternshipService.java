232b93e2cf4bd0d2798be8681ec538d7b4ac46ea
Mays-Alreem <hroubmays@gmail.com>
2024-09-28 23:41:33 +0300

    feat: add an internship service with basic CRUD operations

---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/InternshipService.java
@@ -0,0 +1,23 @@
+package com.gsg.paltechlinker.services;
+
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.gsg.paltechlinker.domain.entities.InternshipEntity;
+
+public interface InternshipService {
+    
+    InternshipEntity save(InternshipEntity internshipEntity);
+
+    InternshipEntity partialUpdate(Long id, InternshipEntity internshipEntity);
+
+    Optional<InternshipEntity> findOne(Long id);
+
+    Page<InternshipEntity> findAll(Pageable pageable);
+
+    void delete(Long id);
+
+    boolean isExists(Long id);
+
+}
