c26bfd9d83abaa14b9551b0f623bf203c3568517
Mays-Alreem <hroubmays@gmail.com>
2024-09-28 23:32:36 +0300

    feat: add InternshipRepository for CRUD operations

---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/repositories/InternshipRepository.java
@@ -0,0 +1,13 @@
+package com.gsg.paltechlinker.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gsg.paltechlinker.domain.entities.CompanyEntity;
+
+@Repository
+public interface InternshipRepository extends CrudRepository<CompanyEntity, Long>,
+                                            PagingAndSortingRepository<CompanyEntity, Long> {    
+    
+}
