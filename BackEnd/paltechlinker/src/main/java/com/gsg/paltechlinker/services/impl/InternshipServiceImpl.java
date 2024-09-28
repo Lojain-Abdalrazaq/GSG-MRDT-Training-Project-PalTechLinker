4b13eccb05282106c9f4177faf9b4b7d43f8e320
Mays-Alreem <hroubmays@gmail.com>
2024-09-28 23:44:18 +0300

    feat: add  InternshipService implementation

---
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/services/impl/InternshipServiceImpl.java
@@ -0,0 +1,51 @@
+package com.gsg.paltechlinker.services.impl;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.gsg.paltechlinker.domain.entities.InternshipEntity;
+import com.gsg.paltechlinker.services.InternshipService;
+
+@Service
+public class InternshipServiceImpl implements InternshipService {
+
+    @Override
+    public InternshipEntity save(InternshipEntity internshipEntity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public InternshipEntity partialUpdate(Long id, InternshipEntity internshipEntity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'partialUpdate'");
+    }
+
+    @Override
+    public Optional<InternshipEntity> findOne(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findOne'");
+    }
+
+    @Override
+    public Page<InternshipEntity> findAll(Pageable pageable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public void delete(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public boolean isExists(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isExists'");
+    }
+    
+}
