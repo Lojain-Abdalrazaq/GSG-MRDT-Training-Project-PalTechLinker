d3187e69c411f1e086fd030255cfcbdf33f363af
Mays-Alreem <hroubmays@gmail.com>
2024-09-29 15:53:34 +0300

    test: verify that create returns http status 201

--- BackEnd/paltechlinker/src/test/java/com/gsg/paltechlinker/controllers/InternshipControllerIntegrationTests.java
+++ BackEnd/paltechlinker/src/test/java/com/gsg/paltechlinker/controllers/InternshipControllerIntegrationTests.java
@@ -1,11 +1,19 @@
 package com.gsg.paltechlinker.controllers;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gsg.paltechlinker.TestDataUtil;
+import com.gsg.paltechlinker.domain.entities.CompanyEntity;
+import com.gsg.paltechlinker.domain.entities.InternshipEntity;
 import com.gsg.paltechlinker.services.InternshipService;
 
 
@@ -23,6 +31,20 @@ public class InternshipControllerIntegrationTests {
         this.mockMvc = mockMvc;
         this.internshipService = internshipService;
         this.objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateInternshipReturnsHttpStatus201() throws Exception {
+        InternshipEntity internshipEntity = TestDataUtil.createTestInternshipEntityA();
+        internshipEntity.setId(null);
+        String internJson = objectMapper.writeValueAsString(internshipEntity);
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/interns/create")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(internJson)
+        ).andExpect(
+            MockMvcResultMatchers.status().isCreated()
+        );
     }
 
 }
