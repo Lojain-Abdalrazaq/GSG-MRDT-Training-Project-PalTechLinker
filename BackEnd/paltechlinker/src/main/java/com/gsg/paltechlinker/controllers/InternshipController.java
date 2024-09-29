140f18f99409e4f8db5f8f427b59af294edb3593
Mays-Alreem <hroubmays@gmail.com>
2024-09-29 14:31:06 +0300

    feat: add post endpoint for creating an internship

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/InternshipController.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/InternshipController.java
@@ -10,6 +10,13 @@ import com.gsg.paltechlinker.mappers.Mapper;
 import com.gsg.paltechlinker.services.CompanyService;
 import com.gsg.paltechlinker.services.InternshipService;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/api/interns")
@@ -24,6 +31,12 @@ public class InternshipController {
         this.mapper = mapper;
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<InternshipDto> create(@RequestBody InternshipDto internshipDto) {
+        InternshipEntity internshipEntity = mapper.mapFrom(internshipDto);
+        InternshipEntity savedInternshipEntity = internshipService.save(internshipEntity);
+        return new ResponseEntity<>(mapper.mapTo(savedInternshipEntity), HttpStatus.CREATED);
+    }
     
     
 }
