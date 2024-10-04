7027a0e54303571429227f825f25e7269644397d
Mays-Alreem <hroubmays@gmail.com>
2024-10-04 14:00:44 +0300

    feat: add endpoint to return internships for given company

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/repositories/InternshipRepository.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/repositories/InternshipRepository.java
@@ -1,14 +1,15 @@
 package com.gsg.paltechlinker.repositories;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
-import com.gsg.paltechlinker.domain.entities.CompanyEntity;
 import com.gsg.paltechlinker.domain.entities.InternshipEntity;
 
 @Repository
 public interface InternshipRepository extends CrudRepository<InternshipEntity, Long>,
                                             PagingAndSortingRepository<InternshipEntity, Long> {    
-    
+
+    List<InternshipEntity> findByCompanyId(Long companyId);
+
 }
