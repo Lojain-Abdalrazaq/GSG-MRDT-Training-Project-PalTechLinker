7a482f2bba7d8d5692ea6112fb4d9cf61659abf3
Mays-Alreem <hroubmays@gmail.com>
2024-10-07 01:31:13 +0300

    feat: generate image urls

--- BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/ImageUploadController.java
+++ BackEnd/paltechlinker/src/main/java/com/gsg/paltechlinker/controllers/ImageUploadController.java
@@ -1,8 +1,13 @@
 package com.gsg.paltechlinker.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +18,7 @@ import java.nio.file.Path;
 import java.nio.file.Files;
 import java.io.IOException;
 import java.nio.file.StandardCopyOption;
-
+import java.net.MalformedURLException;
 
 
 @RestController
@@ -46,5 +51,24 @@ public class ImageUploadController {
 
         return filePath.toString();
     }
+
+    @GetMapping("/images/{filename}")
+    public ResponseEntity<Resource> getImage(@PathVariable String filename) {
+        try {
+            Path filePath = Paths.get(uploadDir).resolve(filename);
+            Resource resource = new UrlResource(filePath.toUri());
+            
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
 
