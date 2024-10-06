62bbe699846e98cb495a69e1bab69d37d7dcf2b2
Maymonakh <maymonakhanfar@gmail.com>
2024-10-06 19:02:37 +0300

    Handle delete internship

--- FrontEnd/paltechlinker/src/Pages/Home/Internships/InternshipCard.jsx
+++ FrontEnd/paltechlinker/src/Pages/Home/Internships/InternshipCard.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { Card, CardContent, Typography, Avatar, Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 import Colors from "../../../Assets/Colors/Colors";
 import CustomButton from "../../../CommonComponents/CustomButton";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const InternshipCard = ({ internship, onClick }) => {
   const storedCompanyId = localStorage.getItem("company_id");
   const navigate = useNavigate();
 
+  const [openDialog, setOpenDialog] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  // Get status styles for internship
   const getStatusStyles = (status) => {
     switch (status) {
       case "OPEN_FOR_APPLICATION":
@@ -75,128 +91,181 @@ const InternshipCard = ({ internship, onClick }) => {
     }
   };
 
-  return (
-    <Card
-      onClick={onClick}
-      sx={{
-        display: "flex",
-        flexDirection: { xs: "column", sm: "row" },
-        padding: "1rem",
-        backgroundColor: Colors.background,
-        height: 350,
-        boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
-        borderRadius: "15px",
-        transition: "transform 0.3s ease, box-shadow 0.3s ease",
-        "&:hover": {
-          transform: "translateY(-5px)",
-          boxShadow: "0 8px 16px rgba(0,0,0,0.2)",
-        },
-      }}
-    >
-      {/* Company Logo */}
-      <Avatar
-        src={internship.company.imageUrl}
-        alt={internship.company.name}
-        sx={{ width: 100, height: 100, marginRight: "1.5rem" }}
-      />
+  // Handle opening the confirmation dialog
+  const handleDeleteClick = () => {
+    setOpenDialog(true);
+  };
 
-      {/* Internship Details */}
-      <CardContent
+  // Handle confirming the deletion
+  const handleConfirmDelete = async () => {
+    setIsDeleting(true);
+    try {
+      const response = await axios.delete(
+        `http://localhost:8081/api/interns/delete/${internship.id}`
+      );
+      if (response.status === 204) {
+        console.log("Internship deleted successfully");
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error deleting internship:", error);
+    } finally {
+      setIsDeleting(false);
+      setOpenDialog(false);
+    }
+  };
+
+  return (
+    <>
+      <Card
+        onClick={onClick}
         sx={{
-          flex: 1,
           display: "flex",
-          flexDirection: "column",
-          gap: 1,
+          flexDirection: { xs: "column", sm: "row" },
+          padding: "1rem",
+          backgroundColor: Colors.background,
+          height: 350,
+          boxShadow: "0 4px 12px rgba(0,0,0,0.1)",
+          borderRadius: "15px",
+          transition: "transform 0.3s ease, box-shadow 0.3s ease",
+          "&:hover": {
+            transform: "translateY(-5px)",
+            boxShadow: "0 8px 16px rgba(0,0,0,0.2)",
+          },
         }}
       >
-        <Typography
-          variant="h6"
-          sx={{
-            fontFamily: "'Cairo', sans-serif",
-            color: Colors.secondary,
-            fontWeight: "bold",
-          }}
-        >
-          {internship.name}
-        </Typography>
+        {/* Company Logo */}
+        <Avatar
+          src={internship.company.imageUrl}
+          alt={internship.company.name}
+          sx={{ width: 100, height: 100, marginRight: "1.5rem" }}
+        />
 
-        <Typography variant="body2" sx={{ fontFamily: "'Cairo', sans-serif" }}>
-          {internship.company.name}
-        </Typography>
-
-        <Typography
-          variant="body2"
+        {/* Internship Details */}
+        <CardContent
           sx={{
-            fontFamily: "'Cairo', sans-serif",
-            color: Colors.primary,
-            fontWeight: "bold",
-          }}
-        >
-          {internship.type}
-        </Typography>
-
-        <Box
-          sx={{
-            display: "inline-block",
-            padding: "0px 20px",
-            borderRadius: "15px",
-            fontFamily: "'Cairo', sans-serif",
-            fontWeight: "bold",
-            ...getStatusStyles(internship.status),
-          }}
-        >
-          {getStatusStyles(internship.status).label}
-        </Box>
-
-        <Typography
-          variant="body2"
-          sx={{
-            marginTop: 5,
-            fontFamily: "'Cairo', sans-serif",
-          }}
-        >
-          {internship.description}
-        </Typography>
-
-        {/* Conditionally Render Buttons */}
-        <Box
-          sx={{
+            flex: 1,
             display: "flex",
-            justifyContent: "flex-start",
-            marginTop: "auto",
+            flexDirection: "column",
+            gap: 1,
           }}
         >
-          {storedCompanyId ? (
-            storedCompanyId == internship.company.id ? (
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  gap: 8,
-                }}
-              >
-                <CustomButton
-                  text="Edit"
-                  fullWidth={false}
-                  onClick={() =>
-                    navigate(`/EditInternShip/${internship.id}`, {
-                      state: { internship_id: internship.id },
-                    })
-                  }
-                />
-                <CustomButton text="Delete" fullWidth={false} />
-              </Box>
-            ) : null
-          ) : (
-            <CustomButton
-              text="Apply Now"
-              onClick={() => window.open(internship.applicationLink, "_blank")}
-            />
-          )}
-        </Box>
-      </CardContent>
-    </Card>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: "'Cairo', sans-serif",
+              color: Colors.secondary,
+              fontWeight: "bold",
+            }}
+          >
+            {internship.name}
+          </Typography>
+
+          <Typography
+            variant="body2"
+            sx={{ fontFamily: "'Cairo', sans-serif" }}
+          >
+            {internship.company.name}
+          </Typography>
+
+          <Typography
+            variant="body2"
+            sx={{
+              fontFamily: "'Cairo', sans-serif",
+              color: Colors.primary,
+              fontWeight: "bold",
+            }}
+          >
+            {internship.type}
+          </Typography>
+
+          <Box
+            sx={{
+              display: "inline-block",
+              padding: "0px 20px",
+              borderRadius: "15px",
+              fontFamily: "'Cairo', sans-serif",
+              fontWeight: "bold",
+              ...getStatusStyles(internship.status),
+            }}
+          >
+            {getStatusStyles(internship.status).label}
+          </Box>
+
+          <Typography
+            variant="body2"
+            sx={{
+              marginTop: 5,
+              fontFamily: "'Cairo', sans-serif",
+            }}
+          >
+            {internship.description}
+          </Typography>
+
+          {/* Conditionally Render Buttons */}
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              marginTop: "auto",
+            }}
+          >
+            {storedCompanyId ? (
+              storedCompanyId == internship.company.id ? (
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    gap: 8,
+                  }}
+                >
+                  <CustomButton
+                    text="Edit"
+                    fullWidth={false}
+                    onClick={() =>
+                      navigate(`/EditInternShip/${internship.id}`, {
+                        state: { internship_id: internship.id },
+                      })
+                    }
+                  />
+                  <CustomButton
+                    text="Delete"
+                    fullWidth={false}
+                    onClick={handleDeleteClick}
+                  />
+                </Box>
+              ) : null
+            ) : (
+              <CustomButton
+                text="Apply Now"
+                onClick={() =>
+                  window.open(internship.applicationLink, "_blank")
+                }
+              />
+            )}
+          </Box>
+        </CardContent>
+      </Card>
+
+      {/* Confirmation Dialog */}
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <DialogTitle>Confirm Delete</DialogTitle>
+        <DialogContent>
+          <DialogContentText sx={{ fontFamily: "'Cairo', sans-serif" }}>
+            Are you sure you want to delete this internship?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <CustomButton text="Cancel" onClick={() => setOpenDialog(false)} />
+          <CustomButton
+            text={isDeleting ? "Deleting..." : "Confirm"}
+            onClick={handleConfirmDelete}
+            disabled={isDeleting}
+          />
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
